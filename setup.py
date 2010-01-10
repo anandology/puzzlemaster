3cecfde7e10a1e1c8fadfa72000170827bbe26b5
Anand Chitipothu <anandology@gmail.com>
2010-01-10 11:28:55 +0530

    use setuptools console_scripts to make puzzlemaster script

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='puzzlemaster',
-      version='0.1',
-      description='Utility to generare, solve and render puzzles',
-      author='Anand Chitipothu',
-      author_email='anandology@gmail.com',
-      url=' http://anandology.github.com/puzzlemaster',
-      packages=['puzzlemaster'],
-      license="",
-      platforms=["any"],
-      scripts=["scripts/puzzlemaster"]
+setup(
+    name='puzzlemaster',
+    version='0.1',
+    description='Utility to generare, solve and render puzzles',
+    author='Anand Chitipothu',
+    author_email='anandology@gmail.com',
+    url=' http://anandology.github.com/puzzlemaster',
+    packages=['puzzlemaster'],
+    license="",
+    platforms=["any"],
+    entry_points = {
+        'console_scripts': [
+            'puzzlemaster = puzzlemaster.cli:main',
+        ]
+    }
  )
 
